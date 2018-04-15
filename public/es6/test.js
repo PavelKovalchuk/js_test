ed2e416afcad141c99e0b3e2e016734ba36b1d5c
PavelKovalchuk <kovpavanat@gmail.com>
2018-04-15 21:28:24 +0300

    es6 - new method

--- public/es6/test.js
+++ public/es6/test.js
@@ -1,12 +1,10 @@
 "use strict";
 
-const sententaces = [
-    {subject: "ES6", verb:"is", object: "sweet"},
-    {subject: "ES5", verb:"is so", object:"last year"}
-];
-function say({subject,verb,object}){
-    console.log(`${subject} ${verb} ${object}`);
-}
-for(let s of sententaces){
-    say(s);
-}
+const o = {
+    name: 'Helen',
+    sayName(){
+        console.log('My name is ' + this.name);
+    }
+};
+
+o.sayName();
