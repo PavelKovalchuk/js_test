5f541f540b44ec4e119a7912d30ddb68bbaa81e6
PavelKovalchuk <kovpavanat@gmail.com>
2018-04-27 18:01:54 +0300

    lazy expressions

---
+++ public/es6/lazy_expressions.js
@@ -0,0 +1,40 @@
+function required(paramName) {
+
+    return false;
+    throw `Parameter ${paramName} is required`;
+
+}
+
+function foo(id = required("id")) {
+
+    console.log('foo is working :', id);
+
+}
+
+foo('ddd');
+foo();
+
+//Gather and Spread
+function boo(...args) {
+
+    console.log('first args : ', args);
+    args.unshift(22);
+    args.unshift(32);
+    bar(...args);
+
+
+
+}
+
+function bar(d, h) {
+    console.log('___bar____');
+    console.log('d : ', d);
+    console.log('h : ', h);
+}
+
+boo();
+
+var x = [1,2,3];
+var y = [4,5];
+var z = [0, ...x, ...y];
+console.log('z : ', z);
