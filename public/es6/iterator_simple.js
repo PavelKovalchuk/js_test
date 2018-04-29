79462ed4af725f828bcc4c44d43c8c2f76cee253
PavelKovalchuk <kovpavanat@gmail.com>
2018-04-29 13:09:57 +0300

    iterators 2

---
+++ public/es6/iterator_simple.js
@@ -0,0 +1,45 @@
+var arr = [1,2,3,4];
+
+var it = arr[Symbol.iterator]();
+
+console.log('arr: ', arr);
+console.log('it: ', it);
+console.log('it.next: ', it.next());
+
+var obj = {
+
+	[Symbol.iterator](){
+		"use strict";
+		 var idx= this.start, end = this.end;
+		 var it = {
+		 	next: () =>{
+
+		 		if(idx <= end){
+
+					var v = this.values[idx];
+					idx++;
+					return{
+						value: v,
+						done: (idx > end)
+					};
+
+				}else {
+		 			return {
+		 				done:true
+					};
+				}
+
+
+			},
+		 };
+
+		 return it;
+	},
+	values: [ 2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32 ],
+	start: 4,
+	end: 13,
+};
+
+var vals = [...obj];
+
+console.log(vals); 
